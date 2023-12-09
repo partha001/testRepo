28a0f90c4640b458e83ab92fee86a744c5ebe941
partha biswas <biswas.partha1@gmail.com>
2023-12-10 03:51:35 +0530

    master-commit000

--- TestApplication/src/main/java/com/partha/TestApplication/App.java
+++ TestApplication/src/main/java/com/partha/TestApplication/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("adding the second line");
-        System.out.println("9");
-        System.out.println("10");
-        System.out.println("this commit 000");
-        System.out.println("this commit 001");
+        System.out.println( "reset" );
     }
 }
