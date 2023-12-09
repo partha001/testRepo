7348a1902063496798e593c814165f2e251ec8af
DESKTOP-S8FEQ2U\biswa <biswas.partha1@gmail.com>
2023-12-10 00:20:47 +0530

    squash test2

--- TestApplication/src/main/java/com/partha/TestApplication/App.java
+++ TestApplication/src/main/java/com/partha/TestApplication/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("adding the second line");
+        System.out.println("1");
+        System.out.println("2");
     }
 }
